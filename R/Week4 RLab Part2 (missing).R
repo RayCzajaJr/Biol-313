c442770734b59f263b5163f01f73d836598edc01
Ray Czaja <121678954+RayCzajaJr@users.noreply.github.com>
2026-02-09 16:19:13 -0800

    Update Week4 RLab Part2 (missing).R

--- R/Week4 RLab Part2 (missing).R
+++ R/Week4 RLab Part2 (missing).R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 
 # we're gonna make a new data frame, a simpler data frame, based on calculations from the OG data frame (MCR_LTER_Coral_Cover_Backreef_Wide_20250429) and we're gonna call this new, simpler data frame 'coral_df'
+
 coral_df <- MCR_LTER_Coral_Cover_Backreef_Wide_20250429 %>% # this funky symbol is how the dplyr package knows we want to continue adding to this code chunk
   group_by(year) %>% # we're telling dplyr to calculate the mean for all values within one 'year'
   summarise(avg_coral = mean(coral, na.rm = TRUE)) # here is where we tell R to calculate the mean, and we tell R to calculate the mean for all 'coral' values
@@ -12,8 +13,9 @@ coral_df <- MCR_LTER_Coral_Cover_Backreef_Wide_20250429 %>% # this funky symbol 
 CoralPlot<-ggplot(coral_df, aes(x = year, y = avg_coral)) +
   geom_point() + # geom_point tells R to that we want our data to be denoted by dots/points (as opposed to say boxes or bars), here you also specify the size and color of the point
   theme_bw()+
-  geom_smooth(se = TRUE, color="aquamarine3") # the simple version here is that geom_smooth (by itself) tells R to make a wiggly/curved line that is 'fit' the points (by fit, I mean captures the general patterns)
-                                                # se tells R to added error bars as a shaded grey region around the fitted line, and we can of course specify the color of the line
+  geom_smooth(method = "lm", se = TRUE, color="aquamarine3")  # geom_smooth (by itself) tells R to make a wiggly/curved line that is 'fit' to the points (by fit, I mean captures the general patterns)
+                                                              # we also specified 'method = lm', 'lm' stands for 'linear model', this means we are telling this function to fit a straight line, as opposed to a wiggly/curved line (which would be the default if we did not specify 'lm')
+                                                              # 'se' tells R to added error bars as a shaded grey region around the fitted line, and we can of course specify the color of the line
 CoralPlot
 
 
